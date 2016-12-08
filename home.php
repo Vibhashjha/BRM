6c5b0330bab2342867aa27f47a0947b132b4ead6
Vibhashjha <vibhashjha75@gmail.com>
2016-12-08 15:36:57 +0530

    Add files via upload

---
+++ home.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>home page</title>
+<link rel="stylesheet" href="viewstyle.css"/>
+</head>
+<body>
+<h1>Book Record Management</h1>
+<a href="insertform.php"> Insert Book</a><br/>
+<a href="view.php"> view Book REcords</a><br/>
+<a href="insertform.php"> Delete Book Records</a><br/>
+<a href="insertform.php"> Update Book Records</a><br/>
+</body>
+</html>
